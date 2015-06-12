2d75ea1c3739539e8a564a2bc995e67954b27128
carlos silva <cefs1984@gmail.com>
2015-06-12 03:23:27 -0300

    Seed: Adding sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+Book.delete_all
+
+users = User.create [
+	{ email: 'admin@gmail.com' , password: '11111111', role: 'admin'},
+	{ email: 'gerente@gmail.com', password: '11111111', role: 'gerente'},
+	{ email: 'funcionario@gmail.com', password: '11111111', role: 'funcionario'},
+	{ email: 'convidado@gmail.com', password: '11111111', role: 'convidado'}
+]
+
+books = Book.create [
+	{ name: 'Rails 1', description: "This file should contain all the record creation needed to seed the database with its default values."},
+	{ name: 'Rails 2', description: "This file should contain all the record creation needed to seed the database with its default values."},
+	{ name: 'Rails 3', description: "This file should contain all the record creation needed to seed the database with its default values."},
+	{ name: 'Rails 4', description: "This file should contain all the record creation needed to seed the database with its default values."}
+]
+
+puts "### #{users.count} Users ###"
+users.each do |user|
+	puts user.email
+end
+
